d475e7b1234b5a49bd30ee8952ec5011e36d8519
leejaehyuk <leehuk0622@gmail.com>
2022-05-19 09:21:52 +0900

    Feat. UseCSVData 생성

---
+++ Game Framework/SDL Game Framework/UseCSVData.c
@@ -0,0 +1,14 @@
+#include "UseCSVData.h"
+#include "stdafx.h"
+
+
+void ReadCSVData(const char* text)
+{
+
+}
+//데이터 항목별로 true false 값 반환하는 함수 들?
+
+bool isTextAct(const char* text)
+{
+
+}
---
+++ Game Framework/SDL Game Framework/UseCSVData.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "stdafx.h"
+#include "Framework.h"
+
+
+void ReadCSVData(const char* text);//데이터 한줄 다 받아오는 함수?
+
+//데이터 항목별로 true false 값 반환하는 함수 들?
+
+bool isTextAct(const char* text);
