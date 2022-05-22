70582e814ba009d5f2280f84e31521c09b47fc25
roomMaker <leehuk0622@gmail.com>
2022-05-22 16:21:18 +0900

    Feat. 5월 22일 16시 21분 메인 합병 수정본

--- Game Framework/SDL Game Framework/Config.h
+++ Game Framework/SDL Game Framework/Config.h
@@ -28,8 +28,8 @@
 #define PAGE_OPTION_POS_X	200
 #define PAGE_OPTION_POS_Y	640
 #define PAGE_OPTION_WIDTH	500
-#define TEXT_MAX_LINE       5
+#define TEXT_MAX_LINE       20
 #define SPACE_BETWEEN_LINES 50
 
 // PageManager 관련 설정
-#define MAX_PAGE_COUNT		1000
+#define MAX_PAGE_COUNT		500
